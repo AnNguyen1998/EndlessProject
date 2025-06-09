9e6f10c582da0b155a0367590302280ec5d5df93
AnNguyen1998 <conandold@gmail.com>
2025-06-09 17:45:55 +0700

    Chapter select

---
+++ assets/Scripts/ChapterSelect/ChapterItem.js
@@ -0,0 +1,30 @@
+const Emitter = require("Emitter");
+const { Game : GameEventKeys } = require('EventKeys');
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        
+    },
+
+    onLoad() {
+        this.init();
+    },
+
+    init() {
+            let checkClear = this.node.getChildByName("IconLock").active;
+            if (!checkClear) {
+                this.node.on(GameEventKeys.CLICK_CHAPTER, this.onClickChapter, this);
+            }
+    },
+
+    onClickChapter() {
+        Emitter.instance.emit(GameEventKeys.SCENE_CHANGE, "Room");
+        Emitter.instance.emit(GameEventKeys.SELECT_CHAPTER, this.node.getChildByName("Level").getComponent(cc.Label).string);
+    },
+
+    onDestroy() {
+        this.node.off(GameEventKeys.CLICK_CHAPTER, this.onClickChapter, this);
+    }
+
+});
