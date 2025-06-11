5ddbbbf026509da6360963f9d6db29ba279fd9b6
SEA.Levie <svpv.hotro@gmail.com>
2025-06-11 11:10:36 +0700

    feat(Player): move PlayerAttributeTemplate to a separate file for better organization and maintainability

--- assets/Scripts/Upgrade/UpgradeController.js
+++ assets/Scripts/Upgrade/UpgradeController.js
@@ -1,30 +1,7 @@
 const Emitter = require('Emitter');
 const { Player: PlayerEventKeys } = require('EventKeys');
 const LocalStorageUnit = require('LocalStorageUnit');
-
-const PlayerAttributeTemplate = {
-    hpLevel: { value: 0, level: 1 },
-    damage: { value: 0, level: 1 },
-    critChance: { value: 0, level: 1 },
-    critRate: { value: 0, level: 1 },
-    attackSpeed: { value: 0, level: 1 },
-    getAttribute(attributeName) {
-        return this[attributeName] ? this[attributeName].value : null;
-    },
-    setAttribute(attributeName, value) {
-        if (this[attributeName]) {
-            this[attributeName].value = value;
-        }
-    },
-    getLevel(attributeName) {
-        return this[attributeName] ? this[attributeName].level : null;
-    },
-    setLevel(attributeName, level) {
-        if (this[attributeName]) {
-            this[attributeName].level = level;
-        }
-    },
-};
+const PlayerAttributeTemplate = require('PlayerAttributeTemplate');
 
 cc.Class({
     extends: cc.Component,
